86204598435829fe72e22ec89656a20d78066c3f
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-10 19:07:45 +0530

    Update listing-card.blade.php

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,20 +1,20 @@
 @props(['listing'])
-<x-card>
- <div class="card">
-                <div class="row no-gutters">
-                    <div class="col-md-4">
-                        <img src="{{ asset('images/hiring.jfif') }}" class="card-img" alt="Listing Image">
 
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h2 class="card-title">
-                                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
-                            </h2>
-                            <p class="card-text">{{$listing->company}}</p>
-                            <p class="card-text">{{$listing->description}}</p>
-                        </div>
-                    </div>
-                </div>
+<div class="card mb-4 shadow-sm">
+    <div class="row no-gutters">
+        <div class="col-md-4">
+            <img src="{{ asset('images/hiring.jfif') }}" class="card-img" alt="Listing Image">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h2 class="card-title">
+                    <a href="/listings/{{ $listing->id }}">{{ $listing->title }}</a>
+                </h2>
+                
+                <x-listing-tags :tagsCsv="$listing->tags" />
+                
+                <p class="card-text"><strong>Company:</strong> {{ $listing->company }}</p>
             </div>
-</x-card>
+        </div>
+    </div>
+</div>
