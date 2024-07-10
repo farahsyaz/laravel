5053b52e2bdbfcdc33e99bd9f662b027f458beeb
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-10 18:33:14 +0530

    Update listing-card.blade.php

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,5 +1,5 @@
 @props(['listing'])
-<x-card class="text-primary">
+<x-card>
  <div class="card">
                 <div class="row no-gutters">
                     <div class="col-md-4">
@@ -13,7 +13,10 @@
                             </h2>
                             <p class="card-text">{{$listing->company}}</p>
                             <p class="card-text">{{$listing->description}}</p>
-                            <span class="badge badge-secondary">{{$listing->tags}}</span>
+                           
+                              <span class="badge badge-primary mr-2 mb-2">Laravel</span>
+            <span class="badge badge-success mr-2 mb-2">API</span>
+            <span class="badge badge-info mr-2 mb-2">React</span>
                         </div>
                     </div>
                 </div>
