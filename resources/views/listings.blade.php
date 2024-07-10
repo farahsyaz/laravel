f9882aab3525716b9f7ca2ddbc2672765f50b013
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-10 19:10:27 +0530

    Update listings.blade.php

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,19 +1,28 @@
 @extends('layout')
-  
-@section('content')
-<!-- Include Hero Partial -->
-    @include('partials._hero')
-<div class="row">
-    <div class="col-md-12">
-        <h1>{{$heading}}</h1>
 
-        @if(count($listings) == 0)
-            <p class="alert alert-danger">No Listings found!</p>
-        @else
-            @foreach($listings as $listing)
-                <x-listing-card :listing="$listing" />
-            @endforeach
-        @endif
+@section('content')
+    <!-- Include Hero Partial -->
+    @include('partials._hero')
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="mb-4">{{ $heading }}</h1>
+
+                @if(count($listings) == 0)
+                    <div class="alert alert-danger" role="alert">
+                        No Listings found!
+                    </div>
+                @else
+                    <div class="row">
+                        @foreach($listings as $listing)
+                            <div class="col-md-6 mb-4">
+                                <x-listing-card :listing="$listing" />
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
+        </div>
     </div>
-</div>
 @endsection
