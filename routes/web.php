6f06de9fce46abc5f8b271f56414546cb1834a37
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-11 10:46:47 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 // All Listins
-Route::get('/', function () {
-    return view('listings',[
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class,'index']);
 
 // Single Listing
-Route::get('/listings/{id}',function($id){
-    return view('listing',[
-        'listing' => Listing::findOrfail($id)
-    ]);
-});
+Route::get('/listings/{listing}',[ListingController::class,'show']);
