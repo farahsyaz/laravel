2a40d17384e1333a7ba2c4b6487b991e6953a094
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-11 13:53:50 +0530

    Update create.blade.php

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -10,37 +10,58 @@
 
                             <div class="form-group">
                                 <label for="title">Title</label>
-                                <input type="text" class="form-control" id="title" name="title" required>
+                                <input type="text" class="form-control" id="title" name="title" >
+                                @error('title')
+                                	<span class="text-danger text-xs mt-1">{{$message}}</span>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="tags">Tags (comma-separated)</label>
                                 <input type="text" class="form-control" id="tags" name="tags">
+                                 @error('tags')
+                                	<span class="text-danger text-xs mt-1">{{$message}}</span>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="company">Company</label>
-                                <input type="text" class="form-control" id="company" name="company" required>
+                                <input type="text" class="form-control" id="company" name="company" >
+                                 @error('company')
+                                	<span class="text-danger text-xs mt-1">{{$message}}</span>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="location">Location</label>
-                                <input type="text" class="form-control" id="location" name="location" required>
+                                <input type="text" class="form-control" id="location" name="location" >
+                                 @error('location')
+                                	<span class="text-danger text-xs mt-1">{{$message}}</span>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" required>
+                                <input type="text" class="form-control" id="email" name="email" >
+                                 @error('email')
+                                	<span class="text-danger text-xs mt-1">{{$message}}</span>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="website">Website</label>
                                 <input type="text" class="form-control" id="website" name="website">
+                                 @error('website')
+                                	<span class="text-danger text-xs mt-1">{{$message}}</span>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="description">Description</label>
-                                <textarea class="form-control" id="description" name="description" rows="5" required></textarea>
+                                <textarea class="form-control" id="description" name="description" rows="5" ></textarea>
+                                 @error('description')
+                                	<span class="text-danger text-xs mt-1">{{$message}}</span>
+                                @enderror
                             </div>
 
                             <button type="submit" class="btn btn-primary">Submit</button>
