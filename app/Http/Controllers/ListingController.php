10b95a1a4c22a59edfa3243c0fd2609e68b05eab
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-11 13:53:54 +0530

    Update ListingController.php

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
 
 class ListingController extends Controller
 {
@@ -20,4 +22,27 @@ class ListingController extends Controller
         'listing' => $listing
     ]);
     }
+
+    //create job post
+    public function create(){
+        return view('listings.create');
+    }
+
+    //store job data
+    public function store(Request $request){
+        $formFields =$request->validate([
+            'title' => 'required',
+            'company' => ['required',Rule::unique('listings','company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required','email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+        
+        return redirect('/');
+    }
+
 }
