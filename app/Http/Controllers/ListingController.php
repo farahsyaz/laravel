a8a2cd6c37855d6c97eaa5bc32ac2becdcb30755
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-11 12:06:04 +0530

    Update ListingController.php

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,8 +9,8 @@ class ListingController extends Controller
 {
     //show all listings
     public function index(){
-            return view('listings.index',[
-        'listings' => Listing::all()
+         return view('listings.index',[
+        'listings' => Listing::latest()->filter(request(['tag','search']))->get()
     ]);
     }
 
