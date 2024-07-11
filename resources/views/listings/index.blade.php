de476ecbf060c086ac0a0ca7af3d0b4625f12be3
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-11 10:46:41 +0530

    Create index.blade.php

---
+++ resources/views/listings/index.blade.php
@@ -0,0 +1,27 @@
+<x-layout>
+    <!-- Include Hero Partial -->
+    @include('partials._hero')
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="mb-4">Job Listings</h1>
+
+                @if(count($listings) == 0)
+                    <div class="alert alert-danger" role="alert">
+                        No Listings found!
+                    </div>
+                @else
+                    <div class="row">
+                        @foreach($listings as $listing)
+                            <div class="col-md-6 mb-4">
+                                <x-listing-card :listing="$listing" />
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+
+</x-layout>
