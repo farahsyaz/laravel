c62da248b7672026920d3240441450d9abda7913
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-12 18:07:51 +0530

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,8 +45,8 @@ class User extends Authenticatable
         ];
     }
 
-    // Relationship with Listings
+   // Relationship with Listings
     public function listings(){
-        return $this->hasMany(Listing::class,'user_id');
+        return $this->hasMany(Listing::class, 'user_id');
     }
 }
