bf370ba9ae084bf5f378ad0fae6e78101f6eb14a
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-31 21:13:28 +0530

    Update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(5)->create();
+        //  User::factory(5)->create();
+        
         $user = User::factory()->create([
             'name' => 'Administrator',
             'email' => 'admin@example.com',
